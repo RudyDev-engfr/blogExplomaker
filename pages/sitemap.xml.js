27daad82c6c1ce84de3a3d5b870e49910ae6716c
Rudy Prevaud <kanachou@rudys-air.home>
2022-12-21 14:00:29 +0100

    Write sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,69 @@
+const SPOT_URL = 'https://www.explomaker.fr/spot'
+const ARTICLE_URL = 'https://www.explomaker.fr/inspiration'
+
+function generateSiteMap(spots, articles) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://www.explomaker.fr/favorites</loc>
+     </url>
+     <url>
+       <loc>https://www.explomaker.fr/inspiration</loc>
+     </url>
+    <url>
+     <loc>https://www.explomaker.fr/404</loc>
+    </url>
+    <url>
+     <loc>https://www.explomaker.fr</loc>
+    </url>
+    <url>
+    <loc>https://www.explomaker.fr/results</loc>
+  </url>
+     ${spots
+       .map(
+         ({ slug }) => `
+       <url>
+           <loc>${`${SPOT_URL}/${slug}`}</loc>
+       </url>
+     `
+       )
+       .join('')}
+       ${articles
+         .map(
+           ({ slug }) => `
+        <url>
+            <loc>${`${ARTICLE_URL}/${slug}`}</loc>
+        </url>
+      `
+         )
+         .join('')}
+   </urlset>
+ `
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const spotRequest = await fetch(SPOT_URL)
+  const articleRequest = await fetch(ARTICLE_URL)
+  const spots = await spotRequest.json()
+  const articles = await articleRequest.json()
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(spots, articles)
+
+  res.setHeader('Content-Type', 'text/xml')
+  // we send the XML to the browser
+  res.write(sitemap)
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default SiteMap
