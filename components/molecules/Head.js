ce881df1ff41dcbad4b261d09b55d72a35cb699c
RudyDev-engfr <rudy.prevaud@gmail.com>
2023-07-26 10:47:08 +0200

    Add constants meta

--- components/molecules/Head.js
+++ components/molecules/Head.js
@@ -49,25 +49,48 @@ const Head = ({ tags }) => {
       })(window,document,'script','dataLayer','GTM-M655582');`}
         </Script>
         {/* End Google Tag Manager */}
+        <meta name="agd-partner-manual-verification" />
 
-        {tags && (
-          <>
-            <title>{tags?.title}</title>
-            <meta name="description" content={tags.description} />
+        <>
+          <title>
+            {tags?.title || 'Explomaker - Planification de voyage simplifiée et personnalisée'}
+          </title>
+          <meta
+            name="description"
+            content={
+              tags.description ||
+              'Avec Explomaker, planifiez votre voyage parfait en quelques clics. Collaborez avec des amis, découvrez des destinations inspirantes et gérez toutes vos informations en un seul endroit. Essayez notre assistant de voyage virtuel pour une expérience encore plus enrichissante.'
+            }
+          />
 
-            {/* Open Graph metadata */}
-            <meta name="agd-partner-manual-verification" />
-            <meta property="og:title" content={`${tags['og:title']} | Explomaker`} />
-            <meta property="og:type" content={tags['og:type']} />
-            <meta property="og:description" content={tags['og:description']} />
-            <meta property="og:url" content={encodeURI(tags['og:url'])} />
-            <meta property="og:site_name" content={tags['og:site_name']} />
-            <meta property="og:image" content={tags['og:image']} />
+          {/* Open Graph metadata */}
+          <meta
+            property="og:title"
+            content={
+              tags['og:title'] || 'Explomaker - Votre assistant de planification de voyage ultime'
+            }
+          />
+          <meta property="og:type" content={tags['og:type'] || 'website'} />
+          <meta
+            property="og:description"
+            content={
+              tags['og:description'] ||
+              'Découvrez Explomaker, une application de planification de voyage innovante qui simplifie le processus de planification, offre des suggestions personnalisées et centralise toutes vos informations de voyage.'
+            }
+          />
+          <meta property="og:url" content={encodeURI(tags['og:url'])} />
+          <meta property="og:site_name" content={tags['og:site_name'] || 'Explomaker'} />
+          <meta
+            property="og:image"
+            content={
+              tags['og:image'] ||
+              'https://storage.googleapis.com/explomaker-data-stateless/2018/12/5f6f7f1e-explomaker-app-logo.jpg'
+            }
+          />
 
-            {/* Other metadata */}
-            <meta name="theme-color" content={theme.palette.primary.main} />
-          </>
-        )}
+          {/* Other metadata */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
+        </>
         {console.log('showmetags', tags)}
       </HeadNext>
     </div>
