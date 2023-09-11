e7109f39a55850e86b9dabe51c4e78c773dc27c3
RudyDev-engfr <rudy.prevaud@gmail.com>
2023-09-11 09:22:49 +0200

    test Google tag Manager moved to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,17 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Google Tag Manager */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-M655582');`}
+          </Script>
+          {/* End Google Tag Manager */}
+        </Head>
         <body
           style={{
             backgroundColor: 'white',
